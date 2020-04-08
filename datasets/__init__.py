50982e81ba398043f7f18cbf6d53f2cf2c1a162b
pgmikhael <pgmikhael@gmail.com>
2020-04-07 23:45:54 -0300

    blg

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,4 +1,4 @@
-from datasets.mnist import MNIST_Dataset
+from datasets.mnist import MNIST
 from datasets.imagenet import Imagenet
 from datasets.cifar import Cifar10
 from datasets.cifar import Cifar100
